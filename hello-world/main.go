5ca88284553d272456ae69cfa52f26d856f373eb
Daniil Maslov <i@dmaslov.dev>
2021-03-18 19:24:20 +0300

    Update test cases and implementation of Hello() func
    Signed-off-by: Daniil Maslov <i@dmaslov.dev>

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 )
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
